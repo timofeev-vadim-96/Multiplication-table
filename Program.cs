78e033ef7d2a68211b19f2d6da8f04131819f005
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-02-07 22:41:26 +0300

    finish

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число , таблицу умн. которого вы хотите увидеть");
+int Numb = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i < Numb; i++)
+{
+    for (int j = 1; j < Numb; j++)
+    {
+        System.Console.Write(i*j + "\t");
+    }
+    System.Console.WriteLine();
+}
+
